888ffea5f24b51c536808fdbedcabc6bb3989c72
Evgeniy Satyev <buddha_cola@Evgeniys-MBP.Dlink>
2021-06-04 00:52:40 +0300

    ex07 working, gotta clean stuff

--- m01/ex07/replace.cpp
+++ m01/ex07/replace.cpp
@@ -6,37 +6,43 @@ int main (int ac, char **av)
 	(void) av;
 	if (ac == 4)
 	{
-		if (!av[1] || !av[2] || !av[3])
+		if (!*av[1] || !*av[2])
 		{
 			std::cout << "empty argument!" << std::endl;
+			return (-1);
 		}
 		std::string		filename = av[1];
 		std::string		to_find = av[2];
 		std::string		to_replace = av[3];
 		std::ifstream	fin;
+		std::ofstream	fout;
 		std::string		current_str;
+		std::string		changed_str;
 		size_t			located;
-		// std::ofstream (filename + ".replace");
 		fin.open(filename, std::fstream::in);
-		if (!fin.is_open())
+		fout.open(filename + ".replace", std::ofstream::out | std::ofstream::trunc);
+		if (!fin.is_open() || !fout.is_open())
 		{
-			std::cout << "file reading error!" << std::endl;
+			std::cout << "file opening error!" << std::endl;
 			return (-1);
 		}
 		else
-			std::cout << filename + " is open!\nto find: " + to_find + "\nto_replace: " + to_replace << std::endl ;
-		while (!fin.eof())
 		{
-			std::getline(fin, current_str);
-			located = current_str.find(to_find);
-			if (located != std::string::npos)
+			while (!fin.eof())
+			{
+				std::getline(fin, current_str);
+				located = current_str.find(to_find);
+				while (located != std::string::npos)
 				{
-					std::cout << "found: " << current_str << std::endl;
-					current_str.replace(located, to_replace.length(), to_replace);
-					std::cout << "changed: " << current_str << std::endl;
+					changed_str = current_str.substr(0, located) + to_replace + current_str.substr(located + to_find.length());
+					current_str = changed_str;
+					located = current_str.find(to_find);
 				}
-		} 
-		fin.close();
+				fout << current_str << std::endl;
+			} 
+			fin.close();
+			fout.close();
+		}
 		return (0);
 	}
 	else

7540d1c644b70be9b31cc7d3fe74892c4f14d518
Wedge Taylor <wtaylor@et-i6.kzn.21-school.ru>
2021-06-04 17:56:43 +0300

    ex07 seem done

--- m01/ex07/replace.cpp
+++ m01/ex07/replace.cpp
@@ -1,6 +1,51 @@
 #include "replace.hpp"
 #include <fstream>
 
+void	replace_loop(std::ifstream &fin, std::ofstream &fout,
+						std::string to_find, std::string to_replace)
+{
+		std::string		current_str;
+		std::string		changed_str;
+		size_t			located;
+
+			while (!fin.eof())
+			{
+				std::getline(fin, current_str);
+				located = current_str.find(to_find);
+				while (located != std::string::npos)
+				{
+					changed_str = current_str.substr(0, located) + to_replace
+							+ current_str.substr(located + to_find.length());
+					current_str = changed_str;
+					located = current_str.find(to_find, located + 1);
+				}
+				fout << current_str << std::endl;
+			} 
+			fin.close();
+			fout.close();
+}
+
+void	replace(char **av)
+{
+		std::string		filename = av[1];
+		std::string		to_find = av[2];
+		std::string		to_replace = av[3];
+		std::ifstream	fin;
+		std::ofstream	fout;
+
+		fin.open(filename, std::fstream::in);
+		if (fin.is_open())
+			fout.open(filename + ".replace", std::ofstream::out | std::ofstream::trunc);
+		if (!fin.is_open() || !fout.is_open())
+		{
+			std::cout << "file opening error!" << std::endl;
+			return ;
+		}
+		else
+			replace_loop(fin, fout, to_find, to_replace);
+		return ;
+}
+
 int main (int ac, char **av)
 {
 	(void) av;
@@ -9,43 +54,12 @@ int main (int ac, char **av)
 		if (!*av[1] || !*av[2])
 		{
 			std::cout << "empty argument!" << std::endl;
-			return (-1);
+			return (1);
 		}
-		std::string		filename = av[1];
-		std::string		to_find = av[2];
-		std::string		to_replace = av[3];
-		std::ifstream	fin;
-		std::ofstream	fout;
-		std::string		current_str;
-		std::string		changed_str;
-		size_t			located;
-		fin.open(filename, std::fstream::in);
-		fout.open(filename + ".replace", std::ofstream::out | std::ofstream::trunc);
-		if (!fin.is_open() || !fout.is_open())
-		{
-			std::cout << "file opening error!" << std::endl;
-			return (-1);
-		}
-		else
-		{
-			while (!fin.eof())
-			{
-				std::getline(fin, current_str);
-				located = current_str.find(to_find);
-				while (located != std::string::npos)
-				{
-					changed_str = current_str.substr(0, located) + to_replace + current_str.substr(located + to_find.length());
-					current_str = changed_str;
-					located = current_str.find(to_find);
-				}
-				fout << current_str << std::endl;
-			} 
-			fin.close();
-			fout.close();
-		}
-		return (0);
+		replace(av);
 	}
 	else
 		std::cout << "wrong number of arguments!" << std::endl;
 	return (0);
 }
+

d9d6fd0a8db5ad42ff878c9825a709da39391956
Wedge Taylor <wtaylor@et-k6.kzn.21-school.ru>
2021-06-17 23:58:09 +0300

    better main in ex03

--- m04/ex03/Character.cpp
+++ m04/ex03/Character.cpp
@@ -18,13 +18,23 @@ void Character::equip(AMateria* m) {
 }
 
 void Character::unequip(int idx) {
+	if (idx < 0 || idx > 3) {
+		std::cout << "wrong slot number!" << std::endl;
+		return ;
+	}
 	if (_inventory[idx])
 		_inventory[idx] = nullptr;
 }
 
 void Character::use(int idx, ICharacter& target) {
+	if (idx < 0 || idx > 3) {
+		std::cout << "wrong slot number!" << std::endl;
+		return ;
+	}
 	if (_inventory[idx])
 		_inventory[idx]->use(target);
+	else
+		std::cout << "empty slot!" << std::endl;
 }
 
 void	Character::setname(std::string name) {
@@ -48,7 +58,8 @@ Character &Character::operator = (const Character&orig) {
 }
 
 Character::Character(std::string const &name) : _name (name) {
-
+	for (int i = 0; i < 4; i++)
+		_inventory[i] = nullptr;
 }
 
 Character::Character() {
--- m04/ex03/main.cpp
+++ m04/ex03/main.cpp
@@ -5,22 +5,33 @@
 
 int main()
 {
+	ICharacter* me = new Character("Олег");
+	ICharacter* bob = new Character("Борис");
+
 	IMateriaSource* src = new MateriaSource();
+
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-	ICharacter* me = new Character("me");
+
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
+	me->equip(tmp);
+	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
-	ICharacter* bob = new Character("bob");
+	tmp = src->createMateria("сure"); // wrong materia name!
+	if (tmp == nullptr)
+		std::cout << "wrong Materia name!" << std::endl;
+	me->use(-69, *bob); // wrong slot number!
 	me->use(0, *bob);
 	me->use(1, *bob);
+	me->use(2, *bob);
+	me->unequip(3);
+	me->use(3, *bob); // empty slot after unequip
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	me->use(3, *bob);
 	delete bob;
 	delete me;
 	delete src;

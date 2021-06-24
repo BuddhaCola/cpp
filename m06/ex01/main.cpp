e8cbdfc524bc4a7ad7318f801a4a70ee15417dce
Evgeniy Satyev <buddha_cola@Evgeniys-MacBook-Pro.local>
2021-06-24 17:26:44 +0300

    started ex02

---
+++ m06/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdint>
+
+typedef	struct {
+	
+}		Data;
+
+uintptr_t serialize(Data* ptr) {
+
+}
+
+Data* deserialize(uintptr_t raw) {
+
+}
+
+int main () {
+
+	return (0);
+}
---
+++ m06/ex02/main.cpp
@@ -0,0 +1,22 @@
+class Base {
+	public:
+		virtual ~Base() {}
+};
+
+class A : public Base {
+};
+
+class B : public Base {
+};
+
+class C : public Base {
+};
+
+Base *generate(void) {
+	
+}
+
+int main () {
+
+	return (0);
+}
